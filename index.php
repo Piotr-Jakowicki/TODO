945f31323ad2f930db483dad89ce306279ea6763
Piotr Jakowicki <jakowicki.piotr@gmail.com>
2020-04-29 11:01:44 +0200

    Add simple template

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php 
 require_once 'core/init.php';
+require_once 'templates/inc/header.php';
 $u = DB::getInstance()->getConnection()->query('select * from tasks');
 foreach($u as $item){
     echo $item['task'];
 }
+require_once 'templates/inc/footer.php';
---
+++ templates/inc/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODO</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a href="#" class="navbar-brand">Brand</a>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+        <div class="navbar-nav">
+            <a href="#" class="nav-item nav-link active">Home</a>
+        </div>
+        <div class="navbar-nav">
+            <!-- Nie zalogowany -->
+            <a href="#" class="nav-item nav-link">Login</a>
+            <a href="#" class="nav-item nav-link">Register</a>
+            <!-- Zalogowany -->
+            <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Account</a>
+                <div class="dropdown-menu">
+                    <a href="#" class="dropdown-item">User details</a>
+                    <a href="#" class="dropdown-item">Change password</a>
+                </div>
+            </div>
+            <a href="#" class="nav-item nav-link">Logout</a>
+            <!-- Endif -->
+        </div>
+        
+    </div>
+</nav>
